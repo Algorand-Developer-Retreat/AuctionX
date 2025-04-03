7f41bdc4e0004bfa8ba4085479a6131f440f38b7
maroti <vaibhavspatre@gmail.com>
2025-04-03 13:10:54 +0530

    added function for application creation and setting state variables on algorand

--- projects/AuctionX-contracts/smart_contracts/auction_x/contract.py
+++ projects/AuctionX-contracts/smart_contracts/auction_x/contract.py
@@ -3,6 +3,28 @@ from algopy.arc4 import abimethod
 
 
 class AuctionX(ARC4Contract):
+    assetid: UInt64
+    floorprice: UInt64
+    highest_bid: UInt64
+    highest_bidder: Account
+
+    # Create the app
+    @abimethod(allow_actions=["NoOp"], create="require")
+    def create_application(self, asset_id: Asset, floor_price: UInt64) -> None:
+        self.assetid = asset_id.id
+        self.floorprice = floor_price
+        self.highest_bid = UInt64(0)
+        self.highest_bidder = Account("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ")
+
+    # Update the floor price
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def set_floor_price(self, floor_price: UInt64) -> None:
+        assert Txn.sender == Global.creator_address
+        self.floorprice = floor_price
+
+
+
+
+
+ 
+
