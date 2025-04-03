3f56213a8c499dcd8f48aa004ecf130465c6054e
maroti <vaibhavspatre@gmail.com>
2025-04-03 13:13:06 +0530

    function added for opt-in the assets for selling and placing the bid for user

--- projects/AuctionX-contracts/smart_contracts/auction_x/contract.py
+++ projects/AuctionX-contracts/smart_contracts/auction_x/contract.py
@@ -22,7 +22,40 @@ class AuctionX(ARC4Contract):
         assert Txn.sender == Global.creator_address
         self.floorprice = floor_price
 
+    # Opt in to the asset that will be sold
+    @abimethod()
+    def opt_in_to_asset(self, mbrpay: gtxn.PaymentTransaction) -> None:
+        assert Txn.sender == Global.creator_address
+        assert not Global.current_application_address.is_opted_in(Asset(self.assetid))
 
+        assert mbrpay.receiver == Global.current_application_address
+        assert mbrpay.amount == Global.min_balance + Global.asset_opt_in_min_balance
+
+        itxn.AssetTransfer(
+            xfer_asset=self.assetid,
+            asset_receiver=Global.current_application_address,
+            asset_amount=0,
+        ).submit()
+
+    # Place a bid
+    @abimethod()
+    def place_bid(self, bid_payment: gtxn.PaymentTransaction) -> None:
+        assert bid_payment.sender == Txn.sender
+        assert bid_payment.receiver == Global.current_application_address
+        assert bid_payment.amount > self.highest_bid
+        assert bid_payment.amount >= self.floorprice
+
+        # Refund the previous highest bidder
+        if self.highest_bidder != Account("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ"):
+            itxn.Payment(
+                receiver=self.highest_bidder,
+                amount=self.highest_bid,
+                fee=1_000,
+            ).submit()
+
+        # Update the highest bid and bidder
+        self.highest_bid = bid_payment.amount
+        self.highest_bidder = Txn.sender
 
 
 
